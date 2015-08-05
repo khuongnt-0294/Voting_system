f1a137e4449735a311e9d47b0bc6c17dea2c5680
KhuongNT <khuongnt1688@gmail.com>
2015-08-05 10:04:18 +0700

    voting system

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,12 @@
 class MoviesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+  before_action :set_movie, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
 
   # GET /movies
   # GET /movies.json
   def index
-    @movies = Movie.all
+    # @movies = Movie.all.order(:cached_votes_score => :desc)
+    @movies = Movie.all.order(:cached_weighted_score => :desc)
   end
 
   # GET /movies/1
@@ -60,6 +61,17 @@ class MoviesController < ApplicationController
       format.html { redirect_to movies_url, notice: 'Movie was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  def upvote
+    @movie.upvote_from current_user, :vote_weight => 3
+    # @movie.upvote_from current_user
+    redirect_to movies_path
+  end
+
+  def downvote
+    @movie.downvote_from current_user
+    redirect_to movies_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   get 'homes/index'
 
-  resources :movies
+  resources :movies do
+    member do
+      put "like" => "movies#upvote"
+      put "unlike" => "movies#downvote"
+    end
+  end  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
